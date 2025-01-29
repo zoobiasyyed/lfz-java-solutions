38bce3b0631a9573284bf414ffd1b55d8702dd8a
Zoobia Syyed <syyedzoobia@gmail.com>
2025-01-28 18:49:24 -0800

    completed java oop

--- java-oop/src/Main.java
+++ java-oop/src/Main.java
@@ -1,17 +1,38 @@
-public class JavaOop {
-    int speed;
-    int size;
-    String color;
+public class Main {
+    private class Cars{
+        private String color;
+        private int numDoors;
 
-    public static void main (String[] args){
+        protected Cars(String color, int numDoors){
+            this.color = color;
+            this.numDoors = numDoors;
+        }
+
+        public String describe(){
+            return "color: " + color + "; number of doors: " + numDoors;
+        }
+    }
+
+    public class Sedan extends Cars{
+        private String sedanColor;
+        private int numOfDoors;
+        public Sedan(String sedanColor, int numOfDoors){
+            super(sedanColor, numOfDoors);
+        }
+
+        @Override
+        public String describe(){
+            return "Sedan " + super.describe();
+        }
+    }
+
+    public static void main(String[] args){
+
+        Main main = new Main();
+        Cars sedan = main.new Sedan("black", 4);
+        System.out.println(sedan.describe());
 
     }
 
-    void accelerate(int amount){
-        speed = speed + amount;
-    }
 
-    void applyBrakes(int amount){
-        speed = speed - amount;
-    }
 }
